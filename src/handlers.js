5b9532662ec32cf0b737a427514c47c248ffa119
parva1474 <parva1474@gmail.com>
2026-08-12 00:54:36 +0330

    Update handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -1372,7 +1372,7 @@ async function handleCallbackQuery(
 
     return;
   }
-}
+
   // ========================================
   // Unknown Callback
   // ========================================
