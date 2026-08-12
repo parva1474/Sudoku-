ed6a4c4420af0f6e3d14b051ca7b980008f4a42f
parva1474 <parva1474@gmail.com>
2026-08-12 18:53:40 +0330

    Update handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -40,7 +40,7 @@ export function createBoardText(game, highlightCell = -1) {
     gridStr += "\n";
     
     if ((row + 1) % 3 === 0 && row < 8) {
-      gridStr += "---------+---------+---------\n";
+      gridStr += "-----------------------+\n";
     }
   }
   
@@ -302,4 +302,4 @@ export async function handleUpdate(update, env) {
   }
 
   return new Response('OK', { status: 200 });
-                       }
+              }
